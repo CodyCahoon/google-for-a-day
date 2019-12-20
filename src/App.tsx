41b99389b9010160c941460434ff160e2e133a2c
Cody Cahoon <codyjcahoon@gmail.com>
2019-12-19 20:52:31 -0500

    Adds component for search result

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import { TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
 import './App.scss';
 import classnames from 'classnames';
+import SearchResult, { ISearchResult } from './components/SearchResult/SearchResult';
 
 const App: React.FC = () => {
-    const [activeTab, setActiveTab] = useState('1');
+    const indexTab = '1';
+    const searchTab = '2';
+    const [activeTab, setActiveTab] = useState(indexTab);
 
     const toggle = (tab: string) => {
         if (activeTab !== tab) {
@@ -12,31 +15,38 @@ const App: React.FC = () => {
         }
     };
 
+    const searchResults: ISearchResult[] = [
+        { title: 'Example', url: 'https://www.google.com', occurrences: 10 },
+        { title: 'Example', url: 'https://www.google.com', occurrences: 10 },
+        { title: 'Example', url: 'https://www.google.com', occurrences: 10 },
+        { title: 'Example', url: 'https://www.google.com', occurrences: 10 },
+    ];
+
+    const renderSearchResult = (s: ISearchResult) => {
+        return <SearchResult title={s.title} url={s.url} occurrences={s.occurrences} />;
+    };
+
     return (
-        <div>
+        <div className={'App'}>
             <Nav tabs>
                 <NavItem>
                     <NavLink
-                        className={classnames({ active: activeTab === '1' })}
-                        onClick={() => {
-                            toggle('1');
-                        }}>
+                        className={classnames({ active: activeTab === indexTab })}
+                        onClick={() => toggle(indexTab)}>
                         Index
                     </NavLink>
                 </NavItem>
                 <NavItem>
                     <NavLink
-                        className={classnames({ active: activeTab === '2' })}
-                        onClick={() => {
-                            toggle('2');
-                        }}>
+                        className={classnames({ active: activeTab === searchTab })}
+                        onClick={() => toggle(searchTab)}>
                         Search
                     </NavLink>
                 </NavItem>
             </Nav>
             <TabContent activeTab={activeTab}>
                 <TabPane tabId="1">Paste a url to index</TabPane>
-                <TabPane tabId="2">Search for indexed pages by word</TabPane>
+                <TabPane tabId="2">{searchResults.map(renderSearchResult)}</TabPane>
             </TabContent>
         </div>
     );
---
+++ src/components/SearchResult/SearchResult.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './SearchResult.scss';
+
+export interface ISearchResult {
+    url: string;
+    title: string;
+    occurrences: number;
+}
+
+const SearchResult = (props: ISearchResult) => {
+    return (
+        <div className={'search-result'}>
+            <h2 className={'search-result__title'}>
+                <a href={props.url} target={'_blank'}>
+                    {props.title}
+                </a>
+            </h2>
+            <span className={'search-result__occurrences'}>{props.occurrences}</span>
+            <span>{props.occurrences === 1 ? ' occurrence' : ' occurrences'}</span>
+        </div>
+    );
+};
+
+export default SearchResult;
