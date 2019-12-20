533f5c4a29959ce8b74e5cac3cdc33b7853dac94
Cody Cahoon <codyjcahoon@gmail.com>
2019-12-19 20:09:41 -0500

    Adds dependency for bootstrap for react

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
 import './App.scss';
+import classnames from 'classnames';
 
 const App: React.FC = () => {
+    const [activeTab, setActiveTab] = useState('1');
+
+    const toggle = (tab: string) => {
+        if (activeTab !== tab) {
+            setActiveTab(tab);
+        }
+    };
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+        <div>
+            <Nav tabs>
+                <NavItem>
+                    <NavLink
+                        className={classnames({ active: activeTab === '1' })}
+                        onClick={() => {
+                            toggle('1');
+                        }}>
+                        Index
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink
+                        className={classnames({ active: activeTab === '2' })}
+                        onClick={() => {
+                            toggle('2');
+                        }}>
+                        Search
+                    </NavLink>
+                </NavItem>
+            </Nav>
+            <TabContent activeTab={activeTab}>
+                <TabPane tabId="1">Paste a url to index</TabPane>
+                <TabPane tabId="2">Search for indexed pages by word</TabPane>
+            </TabContent>
         </div>
     );
 };
