694b06ee0400e109082589125b570b8098305639
Cody Cahoon <codyjcahoon@gmail.com>
2019-12-22 16:03:04 -0500

    Adds test for app.tsx

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a tab for Index', () => {
     const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+    const indexTab = getByText('Index');
+    expect(indexTab).toBeInTheDocument();
+});
+
+test('renders a tab for Search', () => {
+    const { getByText } = render(<App />);
+    const searchTab = getByText('Search');
+    expect(searchTab).toBeInTheDocument();
 });
